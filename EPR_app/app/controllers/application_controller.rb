d1a1a178b1209e407e7514d9d076d0a44f3e764b
b5027327 <b5027327@my.shu.ac.uk>
2019-04-18 15:49:39 +0100

    Users can register/create a user account with restrictions applied so reviews can only be added if the User is registered and logged in.

--- EPR_app/app/controllers/application_controller.rb
+++ EPR_app/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+	private
+	def current_user
+		@current_user ||=User.find(session[ :user_id]) if session[ :user_id]
+	end
+	helper_method :current_user
+	def authorize
+		redirect_to new_session_path, alert: 'In order to add a review, please log in or sign up.' if current_user.nil?
+	end
 end
--- EPR_app/config/routes.rb
+++ EPR_app/config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
  resources :ipads
  resources :iwatches
  resources :profiles
+ resources :users
+ resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
